f3a06035b5e473d87c4399bc62dc2f695d59911f
han.namoo <han.namoo@gmail.com>
2019-12-31 15:21:36 -0800

    lesson 3

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager { get; set; }
+        private readonly SignInManager<ApplicationUser> _signInManager { get; set; }
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+        }
+    }
+}
